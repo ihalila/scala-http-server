46b547834cde968e0a7ca46516d1b3aa3654cfff
Ilkka Halila <ilkka@hali.la>
2018-03-18 10:23:41 +0200

    Added test to verify that two consecutive GETs are parsed correctly

--- src/test/scala/la/hali/HttpRequestSpec.scala
+++ src/test/scala/la/hali/HttpRequestSpec.scala
@@ -35,4 +35,28 @@ class HttpRequestSpec extends FlatSpec with Matchers {
         }
     }
   }
+
+  it should "parse two consecutive GETs" in {
+    val stringBuilder =
+      new StringBuilder()
+        .append("GET /hello.txt HTTP/1.1\r\n")
+        .append("User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n")
+        .append("Host: www.example.com\r\n")
+        .append("Accept-Language: en, mi\r\n")
+        .append("\r\n")
+        .append("GET /foo/bar HTTP/1.1\r\n")
+        .append("User-Agent: curl/7.16.3 libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3\r\n")
+        .append("Host: www.example.com\r\n")
+        .append("Accept-Language: en, mi\r\n")
+        .append("\r\n")
+
+    val requestString = stringBuilder.toString()
+    val reqBytes = ArrayBuffer(requestString.getBytes(StandardCharsets.US_ASCII): _*)
+
+    val Some((req1, rem)) = HttpRequest.fromBytes(reqBytes)
+    val Some((req2, _)) = HttpRequest.fromBytes(rem)
+
+    req1.path shouldBe "/hello.txt"
+    req2.path shouldBe "/foo/bar"
+  }
 }
