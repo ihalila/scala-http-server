cf01f9ad97d4c67be1d25f86a6c887b2585c4d9b
Ilkka Halila <ilkka@hali.la>
2018-03-19 20:54:54 +0200

    Raise reuse: Refactor response routines

--- src/main/scala/la/hali/HttpResponse.scala
+++ src/main/scala/la/hali/HttpResponse.scala
@@ -8,81 +8,50 @@ trait HttpResponse {
   def toBytes: Array[Byte]
 }
 
-object NotImplementedResponse extends HttpResponse {
-  def toBytes: Array[Byte] = {
-    val message = "Not implemented"
+object HttpResponse {
+  def defaultHeaders: String = {
     val date = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneId.of("UTC")).format(Instant.now())
-    val stringBuilder = new StringBuilder()
-      .append("HTTP/1.1 501 Not Implemented\r\n")
-      .append(s"Date: $date\r\n")
-      .append("Server: la.hali.scala-http-server\r\n")
-      .append(s"Content-Length: ${message.getBytes(StandardCharsets.US_ASCII).length}\r\n")
-      .append("Content-Type: text/plain\r\n")
-      .append("\r\n")
-      .append(message)
-    stringBuilder.toString().getBytes(StandardCharsets.US_ASCII)
+    s"Date: $date\r\n" +
+      "Server: la.hali.scala-http-server\r\n"
   }
+
+  def contentHeaders(body: String): String = {
+    s"Content-Length: ${body.getBytes(StandardCharsets.US_ASCII).length}\r\n" +
+      "Content-Type: text/plain\r\n"
+  }
+
+  def toBytes(response: String, message: Option[String]): Array[Byte] = {
+    new StringBuilder(s"HTTP/1.1 $response\r\n")
+      .append(HttpResponse.defaultHeaders)
+      .append(message.map(contentHeaders).getOrElse(""))
+      .append("\r\n")
+      .append(message.getOrElse(""))
+      .toString()
+      .getBytes(StandardCharsets.US_ASCII)
+  }
+}
+
+object NotImplementedResponse extends HttpResponse {
+  def toBytes: Array[Byte] =
+    HttpResponse.toBytes("501 Not Implemented", Some("501 - Not Implemented"))
 }
 
 object BadRequestResponse extends HttpResponse {
-  def toBytes: Array[Byte] = {
-    val message = "400 - Bad Request"
-    val date = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneId.of("UTC")).format(Instant.now())
-    val stringBuilder = new StringBuilder()
-      .append("HTTP/1.1 404 Bad Request\r\n")
-      .append(s"Date: $date\r\n")
-      .append("Server: la.hali.scala-http-server\r\n")
-      .append(s"Content-Length: ${message.getBytes(StandardCharsets.US_ASCII).length}\r\n")
-      .append("Content-Type: text/plain\r\n")
-      .append("\r\n")
-      .append(message)
-    stringBuilder.toString().getBytes(StandardCharsets.US_ASCII)
-  }
+  def toBytes: Array[Byte] =
+    HttpResponse.toBytes("404 Bad Request", Some("400 - Bad Request"))
 }
 
 object NotFoundResponse extends HttpResponse {
-  def toBytes: Array[Byte] = {
-    val message = "404 - Not Found"
-    val date = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneId.of("UTC")).format(Instant.now())
-    val stringBuilder = new StringBuilder()
-      .append("HTTP/1.1 404 Not Found\r\n")
-      .append(s"Date: $date\r\n")
-      .append("Server: la.hali.scala-http-server\r\n")
-      .append(s"Content-Length: ${message.getBytes(StandardCharsets.US_ASCII).length}\r\n")
-      .append("Content-Type: text/plain\r\n")
-      .append("\r\n")
-      .append(message)
-    stringBuilder.toString().getBytes(StandardCharsets.US_ASCII)
-  }
+  def toBytes: Array[Byte] =
+    HttpResponse.toBytes("404 Not Found", Some("404 - Not Found"))
 }
 
 case class ServerErrorResponse(throwable: Throwable) extends HttpResponse {
-  def toBytes: Array[Byte] = {
-    val message = s"500 - Internal Server Error\n$throwable"
-    val date = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneId.of("UTC")).format(Instant.now())
-    val stringBuilder = new StringBuilder()
-      .append("HTTP/1.1 500 Internal Server Error\r\n")
-      .append(s"Date: $date\r\n")
-      .append("Server: la.hali.scala-http-server\r\n")
-      .append(s"Content-Length: ${message.getBytes(StandardCharsets.US_ASCII).length}\r\n")
-      .append("Content-Type: text/plain\r\n")
-      .append("\r\n")
-      .append(message)
-    stringBuilder.toString().getBytes(StandardCharsets.US_ASCII)
-  }
+  def toBytes: Array[Byte] =
+    HttpResponse.toBytes("500 Internal Server Error", Some(s"500 - Internal Server Error\n$throwable"))
 }
 
 case class OKResponse(content: String) extends HttpResponse {
-  def toBytes: Array[Byte] = {
-    val date = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneId.of("UTC")).format(Instant.now())
-    val stringBuilder = new StringBuilder()
-      .append("HTTP/1.1 200 OK\r\n")
-      .append(s"Date: $date\r\n")
-      .append("Server: la.hali.scala-http-server\r\n")
-      .append(s"Content-Length: ${content.getBytes(StandardCharsets.US_ASCII).length}\r\n")
-      .append("Content-Type: text/plain\r\n")
-      .append("\r\n")
-      .append(content)
-    stringBuilder.toString().getBytes(StandardCharsets.US_ASCII)
-  }
+  def toBytes: Array[Byte] =
+    HttpResponse.toBytes("200 OK", Some(content))
 }
